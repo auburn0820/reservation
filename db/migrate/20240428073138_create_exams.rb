0603d3909e8ec9a00c0111edd3172e264b36d9e5
suyeong <auburn0820@gmail.com>
2024-04-28 17:35:14 +0900

    feat: exam 모델 마이그레이션 파일 추가

---
+++ db/migrate/20240428073138_create_exams.rb
@@ -0,0 +1,13 @@
+class CreateExams < ActiveRecord::Migration[7.1]
+  def change
+    create_table :exams do |t|
+      t.string :exam_id, null: false
+      t.string :name, null: false
+      t.datetime :started_at, null: false
+      t.datetime :ended_at, null: false
+      t.timestamps
+    end
+
+    add_index :exams, :exam_id, unique: true
+  end
+end
