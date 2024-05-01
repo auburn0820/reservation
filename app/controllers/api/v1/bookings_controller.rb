e16b5739a2ca43ab17fdf6b2c00edfbd5b9c4cb1
피수영 <vltndud123@naver.com>
2024-05-02 02:44:29 +0900

    feat: 예약 수정 API 추가

--- app/controllers/api/v1/bookings_controller.rb
+++ app/controllers/api/v1/bookings_controller.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Api::V1::BookingsController < ApplicationController
-  before_action :set_booking, only: [:show, :destroy, :confirm]
+  before_action :set_booking, only: [:show, :destroy, :confirm, :update]
   before_action :check_exam_availability, only: [:create]
+  before_action :check_cancel_exam_availability, only: [:destroy]
 
   def index
     @bookings = current_user.admin? ? Booking.all : Booking.where(user_id: current_user.user_id)
@@ -13,6 +14,18 @@ class Api::V1::BookingsController < ApplicationController
 
   def show
     render_json_response(data: @booking)
+  end
+
+  def update
+    booking_params.tap do |params|
+      return render_json_response(message: "Cannot found exam", status: 404) unless Exam.exists?(exam_id: params[:exam_id])
+    end
+
+    if @booking.change_exam(booking_params[:exam_id])
+      render_json_response(data: @booking)
+    else
+      render_json_response(message: @booking.errors.full_messages.join(', '), status: 422)
+    end
   end
 
   def create
@@ -26,8 +39,6 @@ class Api::V1::BookingsController < ApplicationController
   end
 
   def destroy
-    return render_json_response(message: "Only the person who made the booking can cancel it.", status: 400) unless authorized_to_cancel?
-
     if @booking.cancel
       render_json_response(data: @booking)
     else
@@ -48,20 +59,24 @@ class Api::V1::BookingsController < ApplicationController
   private
 
   def set_booking
-    @booking = Booking.find_by(booking_id: params[:id])
+    @booking = Booking.find_by(booking_id: params[:id], user_id: current_user.user_id)
     render_json_response(message: 'Booking not found.', status: 404) unless @booking
   end
 
   def authorized_to_cancel?
-    current_user.admin? || @booking.user_id == current_user.user_id && @booking.status != "confirmed"
+    current_user.admin? || @booking.user_id == current_user.user_id
+  end
+
+  def check_cancel_exam_availability
+    return render_json_response(message: "Only the person who made the booking can cancel it.", status: 401) unless authorized_to_cancel?
+    return render_json_response(message: "Already canceled.", status: 400) if @booking.is_canceled?
+    return render_json_response(message: "You can only cancel a booking before it is confirmed.", status: 400) unless @booking.is_cancelable_status?
   end
 
   def check_exam_availability
-    render_json_response(message: "Can't booking.", status: 422) unless can_book_exam?
-  end
-
-  def can_book_exam?
-    Booking.where(exam_id: booking_params[:exam_id], status: "confirmed").count < Constants::BOOKING_LIMIT_COUNT
+    return render_json_response(message: "Already book the exam.", status: 422) if Booking.is_already_booked?(user_id: current_user.user_id, exam_id: booking_params[:exam_id])
+    return render_json_response(message: "Booking must be made at least 3 days in advance.", status: 422) unless Exam.find_by(exam_id: booking_params[:exam_id]).can_apply_on_date?
+    return render_json_response(message: "All spots are booked for this time slot.", status: 422) unless Booking.can_reserve_seat?(booking_params[:exam_id])
   end
 
   def booking_params
